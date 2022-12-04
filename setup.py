a981091bfe5fd8e9d99e2dad69835043b0e9dd7d
GabrielMissael <missaelgabo@gmail.com>
2022-12-04 11:55:42 -0600

    Add setup to build package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from setuptools import setup, find_packages
+
+setup(
+    package_dir={'': './unblind'}
+)
